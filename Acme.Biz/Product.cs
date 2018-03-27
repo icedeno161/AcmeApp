69343efddf9fe73e58b4ab163a618f067adeb9c8
Ivan Cedeño <icedeno@gmail.com>
2018-03-26 20:27:01 -0400

    Working with classes

---
+++ Acme.Biz/Product.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acme.Biz
+{
+    /// <summary>
+    /// Manages the products in our inventory.
+    /// </summary>
+    public class Product
+    {
+        private string productName;
+        private string description;
+        private int productId;
+
+        public string ProductName
+        {
+            get { return productName; }
+            set { productName = value; }
+        }
+
+        public string Description
+        {
+            get { return description; }
+            set { description = value; }
+        }
+
+        public int ProductId
+        {
+            get { return productId; }
+            set { productId = value; }
+        }
+
+        public string SayHello()
+        {
+            return $"Hello {ProductName} ({ProductId}): {Description}";
+        }
+    }
+}
